2b5492eb6b7be56d61f342bea8b18ad43ff80c12
Manpreet Bahl <manpreetsingh.bahl@gmail.com>
2018-07-11 20:16:51 -0700

    Search unanswered questions of a site

--- api_calls.py
+++ api_calls.py
@@ -9,19 +9,16 @@ baseURL = "https://api.stackexchange.com/2.2/"
 #================Get the list of sites==================
 def getSites():
     page = 1
-
-    params = {"key": APIKEY, "page": page, "filter": "!6P.ZZU_Ynt.HP"}
-    res = requests.get(baseURL + "sites", params=params)
-    resJSON = res.json()
-    sites = resJSON['items']
-    hasMore = resJSON['has_more']
+    sites = []
+    hasMore = True
 
     while hasMore:
         params = {"key": APIKEY, "page": page, "filter": "!6P.ZZU_Ynt.HP"}
         res = requests.get(baseURL + "sites", params=params)
         if res.status_code == 200:
             resJSON = res.json()
-            sites.append(resJSON['items'])
+            for item in resJSON['items']:
+                sites.append(item)
             hasMore = resJSON['has_more']
             page += 1
         else:
@@ -29,8 +26,39 @@ def getSites():
             sys.exit(1)
     
     return sites
+#=======================================================
 
-print(getSites())
+def getNoAnswerQuestions(site, min, max):
+    page = 1
+    questions = []
+    hasMore = True
 
+    while hasMore:
+        params = {
+            "key": APIKEY, 
+            "page": page, 
+            "order": "desc",
+            "sort": "creation",
+            "site": site,
+            "min": min,
+            "max": max
+        }
+
+        res = requests.get(baseURL + "/questions/no-answers", params=params)
+        if res.status_code == 200:
+            resJSON = res.json()
+            for item in resJSON['items']:
+                questions.append(item)
+            hasMore = resJSON['has_more']
+            page += 1
+        else:
+            print("An error has occured when making API call")
+            sys.exit(1)
+    
+    return questions
+
+
+#print(getSites())
+print(getNoAnswerQuestions("stackoverflow", 1531180800, 1531267200))
 
 
